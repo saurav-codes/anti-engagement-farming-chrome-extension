b9672b10fd2dba79059416539b1d946d589e127f
selftaughtdev <selftaughtdev.me@gmail.com>
2026-09-13 08:49:48 +0530

    refactor(backend): remove Django backend superseded by FastAPI server

--- backend/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),  # Home page view
-    path('classify/', views.ClassifyView.as_view(), name='classify'),  # Classification endpoint
-]
